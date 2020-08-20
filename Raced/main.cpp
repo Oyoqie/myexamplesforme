018c419707d679c48c743ff50e25e35e564018b4
Yoqie <44141075+Oyoqie@users.noreply.github.com>
2020-08-21 01:27:43 +0300

    Add files via upload

---
+++ Raced/main.cpp
@@ -0,0 +1,45 @@
+#ifdef WIN64 || _WIN32
+	#include <windows.h>
+#endif
+
+#include <stdio.h>
+#include <chrono>
+#include <omp.h>
+
+#define S 0x004fffff
+
+typedef unsigned int UINT;
+
+int main() {
+	UINT result = 0;
+	UINT h = 32 / omp_get_max_threads();
+
+	#pragma omp parallel num_threads(omp_get_max_threads())
+	{
+		for (UINT j = 0; j < S; ++j) {
+			UINT t = omp_get_thread_num();
+			//#pragma omp atomic
+			result ^= (0x1 << (j % 2)) << t * h;
+		}
+	}
+
+    switch (result) {
+    	case 0x55555555:
+    		printf("0x55555555 fine.\n");
+    		break;
+    	case 0xffffffff:
+    		printf("0xffffffff fine.\n");
+    		break;
+    	case 0xaaaaaaaa:
+    		printf("0xaaaaaaaa fine.\n");
+    		break;
+    	case 0:
+    		printf("0x0 fine.\n");
+    		break;
+    	default:
+    		printf("Got Race condition %x\n", result);
+    		break;
+    }
+
+	return 0;
+}
