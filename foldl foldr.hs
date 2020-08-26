2d1fa9599acfab61ddc221db875bf124d9ca1095
Yoqie <44141075+Oyoqie@users.noreply.github.com>
2020-08-26 22:25:36 +0300

    Update foldl foldr.hs

--- foldl foldr.hs
+++ foldl foldr.hs
@@ -1,6 +1,6 @@
-foldr f z []	= z
-foldr f z x:xs	= x `f` foldr f z xs
+foldr f z []		= z
+foldr f z (x:xs)	= x `f` foldr f z xs
 
-foldl f z []	= z
-foldl f z x:xs	= let z' = z `f` x
-				  in foldl f z' xs
+foldl f z []		= z
+foldl f z (x:xs)	= let z' = z `f` x
+			  in foldl f z' xs
