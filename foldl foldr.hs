506a3a7a725dd2cfc802a374266440754e0f0933
Yoqie <44141075+Oyoqie@users.noreply.github.com>
2020-08-26 22:28:02 +0300

    Update foldl foldr.hs

--- foldl foldr.hs
+++ foldl foldr.hs
@@ -1,6 +1,12 @@
 foldr f z []		= z
 foldr f z (x:xs)	= x `f` foldr f z xs
 
+-- foldr (+) z x:xs
+-- (x1 + (x2 + ...(xn + z)))
+
 foldl f z []		= z
 foldl f z (x:xs)	= let z' = z `f` x
 			  in foldl f z' xs
+
+-- foldl (+) z x:xs
+-- (((z + x1) + x2)... + xn)
